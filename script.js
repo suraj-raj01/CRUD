b945500788411ad70c3e34868ab7d5bb4f5069fe
SURAJ_KUMAR <surajk38656@gmail.com>
2024-08-17 16:13:59 +0530

    PHP_CRUD

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function mode() {
     document.body.classList.toggle('demo');
 }
 
+$(document).ready(function(){
+    $('#navbar').slideDown(1000);
+    $('#hero').slideDown(1000);
+    $('#display').slideDown(1000);
+})
